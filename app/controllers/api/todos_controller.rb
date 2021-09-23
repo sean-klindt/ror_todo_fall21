2e2101213096f84563d9c709d2a803b9379faaa5
Sean Klindt <sk.klindt@gmail.com>
2021-09-22 21:01:25 -0600

    actions

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -7,4 +7,42 @@ class Api::TodosController < ApplicationController
   # return data, html, xml json
     # json object notation for passing data
     # can be read in both languages
+
+  def index
+    render json: Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: { errors: @todo.errors }, status: :unprocessable_entity
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: { errors: @todo.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Todo.find(params[:id]).destroy
+    render json: { message: 'Todo Deleted' }
+  end
+
+  private
+    # { todo: { title: "", complete: false }}
+    def todo_params
+      params.require(:todo).permit(:title, :complete)
+    end
+
 end
